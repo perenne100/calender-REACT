07ce128981b0ae923bd459bfa7ebdb92a20386fa
perenne100 <148433978+perenne100@users.noreply.github.com>
2023-12-31 03:08:17 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calendar from './Calendar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Simple React Calendar</h1>
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
